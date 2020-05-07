f6ad7b5c65f76c87bd43b5f40c5cc4e7360667fa
Joel Scott <jscottusf@gmail.com>
2020-05-06 22:56:15 -0400

    C# files

---
+++ GithubEmojis/Emoji.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace GithubEmojis
+{
+    public class Emoji
+    {
+        public string Key
+        {
+            get;
+            set;
+        }
+        public string Url
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ GithubEmojis/Pages/Index.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace GithubEmojis.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private IGithubEmojiService _emojiService;
+
+        public IndexModel(IGithubEmojiService emojiSvc)
+        {
+            _emojiService = emojiSvc;
+        }
+
+        public IList<Emoji> Emojis
+        {
+            get;
+            set;
+        }
+
+        public async Task OnGet()
+        {
+            Emojis = await _emojiService.GetEmojis();
+        }
+    }
+}
